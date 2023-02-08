37da37d4ea1681a0f0a110f7329d3dbeca344092
PavitraAgarwal <85789615+PavitraAgarwal21@users.noreply.github.com>
2023-02-08 18:12:36 +0530

    Update FunctionError.sol

--- FunctionError.sol
+++ FunctionError.sol
@@ -2,20 +2,15 @@
 pragma solidity ^0.8.17;
 contract FunctionError 
     {
-        
         // setting the owner of the contract 
         //it once the contract is deployed it cannot changed 
-        //because it only execute at once duering the deployment 
+        //because it only execute at once during the deployment 
         address  payable public owner ;
         constructor () {
         owner = payable(msg.sender);
         }
-
-
         uint public i ;  // another state variable 
-        
         //require message 
-
         // if the require has two argument , 
         // first -  is the condition 
         // second - is the message if the first argument condition is false 
@@ -23,23 +18,20 @@ contract FunctionError
         //                              and terminate the execution of the function 
         // it also excute without the second argument        
         function _Require() public view {
-                require(msg.sender == owner,"not the owner of the contract "); 
+                require(msg.sender == owner,"not the owner of the contract from require"); 
         
         } 
-
-
         // require is uses for complex errors handlings 
         // ths the same implication as the above 
         // they are similar to their meaning in the english language 
         //revert return the remmaing gas  which is left 
         // revert is safer to use 
-
         //revert is prefer to use  - bcz if some error has occured then instead 
         //                           of burn all the gas it revert back the remmaing gas  
         function _revert() public  view {
             if(msg.sender != owner ) { 
                 // revert is used for complex 
-                revert ("less than or equal to 5");
+                revert ("not the owner of the contract from the revert");
             }
         }
 
@@ -56,6 +48,7 @@ contract FunctionError
                 revert NotOwner({_address: payable(msg.sender), str : "not the owner address"});
               }
         }
+        // custom error can also provide who have call the function by sending the msg.sender to the log
 
         // assert is simple to use have only one argument
         //if it false it terminates the execution of the function 
