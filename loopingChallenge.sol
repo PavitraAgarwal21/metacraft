a0d00cd9b065b399145d6338312893cd71a8e578
PavitraAgarwal <85789615+PavitraAgarwal21@users.noreply.github.com>
2023-02-09 22:52:47 +0530

    Create loopingChallenge.sol

---
+++ loopingChallenge.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+contract Calculator {
+   function divide(int a, int b) public pure returns (int) {
+        require(b != 0, "Cannot divide by zero");
+        return a / b;
+    }
+     function subtract(int a, int b) public pure returns (int) {
+        return a - b;
+    }
+     function add(int a, int b) public pure returns (int) {
+        return a + b;
+    }
+    function multiply(int a, int b) public pure returns (int) {
+        return a * b;
+    }
+}
+
