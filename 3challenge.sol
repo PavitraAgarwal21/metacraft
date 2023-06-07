dc960bb56debee34a8a3f7abd75b9146cb41f946
PavitraAgarwal <85789615+PavitraAgarwal21@users.noreply.github.com>
2023-06-07 19:52:07 +0530

    Update 3challenge.sol

--- 3challenge.sol
+++ 3challenge.sol
@@ -2,13 +2,20 @@
 pragma solidity >=0.8.0 <0.9.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.8/contracts/token/ERC20/ERC20.sol";
 contract MyToken is ERC20 {
-
-constructor (string memory name , string memory symbol ,  uint256 initialSupply) ERC20(name , symbol)  {
-    _mint(msg.sender , initialSupply * (10**18) );
+address owner ;
+constructor (string memory name , string memory symbol ) ERC20(name , symbol)  {
+  owner =msg.sender ;
 }
-
+function mint (uint amount) public  {
+require(msg.sender == owner , "not the owner of the contract " );
+     _mint(msg.sender , amount  );
+}
 function decimals() public view virtual override returns (uint8) {
   return 18;
+}
+
+function burn(uint _amount) public  {
+    _burn(msg.sender , _amount) ;
 }
 
 }
